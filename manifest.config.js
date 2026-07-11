b4a22483bf188daa480c673d91dc3f9813bd91a3
Anees Memon <memonanees277@gmail.com>
2026-07-11 20:24:30 +0500

    Folder Structure

---
+++ manifest.config.js
@@ -0,0 +1,19 @@
+export default {
+    manifest_version: 3,
+
+    name: "Extractly",
+
+    description: "Extract text from screenshots and images using OCR.",
+
+    version: "1.0.0",
+
+    action: {
+        default_popup: "src/popup/popup.html"
+    },
+
+    icons: {
+        16: "icon16.png",
+        48: "icon48.png",
+        128: "icon128.png"
+    }
+};
